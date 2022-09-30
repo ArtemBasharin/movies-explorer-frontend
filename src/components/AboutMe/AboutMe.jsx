a4d9f5d44c0967c94bac432c7371823c45cfc3c2
basharinaa <temon-bash@yandex.ru>
2022-10-01 02:29:06 +0700

    some edit added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -9,8 +9,12 @@ export default function AboutMe() {
         <div className="aboutme__biography-container">
           <div className="aboutme__biography">
             <h3 className="aboutme__name">Artem Basharin</h3>
-            <p className="aboutme__age">Frontend developer, {new Date().getFullYear()-1985} лет</p>
-            <p className="aboutme__text"> I Was born in Siberia, now i'm working in PJSC "Russian Railways" and learn frontend and some backend development</p>
+            <p className="aboutme__age">Frontend developer, {new Date().getFullYear()-1985} years</p>
+            <p className="aboutme__text"> I was born in Siberia, now i'm working in PJSC "Russian Railways" and learn frontend and some backend development in Yandex.Praktikum.
+            I'm not new to development. I had my own microcontroller programming projects in C/C++. I also created a functional sample for an automatic irrigation and climate control system.
+            As a side job, I design 3D models in CAD applications and prototype plastic on my FDM 3D printer. One of the expensive projects was the design and manufacture of a commercial model
+            of a 4-axis CNC furniture machine with a working field of 3 by 2 meters.
+            </p>
             <ul className="aboutme__contacts">
               <li>
                 <a href="https://github.com/ArtemBasharin" className="aboutme__contact-link" target="_blank" rel="noreferrer">Github</a>
--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,23 +1,27 @@
 import './Promo.css';
-import logo from '../../images/landing-logo.svg';
+
 
 export default function Promo() {
   return (
     <section className="promo">
       <div className="promo__container">
-        <div className="promo__about-project">
+
           <h1 className="promo__title">
-            Учебный проект студента факультета Веб&#8209;разработки.
+            Учебный проект студента факультета&nbsp;Веб&#8209;разработки.
           </h1>
-          <p className="promo__description">
-            Листайте ниже, чтобы узнать больше про этот проект и его создателя.
-          </p>
+          <div className='promo__link-container'>
           <a href="https://goo.su/P65u" target="_blank" rel="noopener noreferrer" className="promo__learn-more-link" >
-          Узнать больше
+          О проекте
           </a>
+          <a href="https://goo.su/P65u" target="_blank" rel="noopener noreferrer" className="promo__learn-more-link" >
+          Технологии
+          </a>
+          <a href="https://goo.su/P65u" target="_blank" rel="noopener noreferrer" className="promo__learn-more-link" >
+          Студент
+          </a>
+          </div>
         </div>
-        <img src={logo} alt="логотип - Земной шар" className="promo__logo" />
-      </div>
+
     </section>
   );
 }
