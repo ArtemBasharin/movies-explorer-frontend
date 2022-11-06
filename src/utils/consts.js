18b51806f3ce645a732f0c6591513da0cd576468
basharinaa <temon-bash@yandex.ru>
2022-11-07 00:09:30 +0700

    bug fixes

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,4 +1,4 @@
-const API_URL = "http://api.movex.nomoredomains.sbs";
+const API_URL = "https://api.movex.nomoredomains.sbs";
 // const API_URL = "http://localhost:3000";
 const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
 const SHORTMOVIES_DURATION = 40;
