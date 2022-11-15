22febc0a9f41c813a57355b7e3e3f56d46ee58a1
basharinaa <temon-bash@yandex.ru>
2022-11-15 19:30:32 +0600

    create MoviesApi client

---
+++ src/api/MoviesApi.js
@@ -0,0 +1,25 @@
+export const MOVIES_URL = "https://api.nomoreparties.co/beatfilm-movies";
+
+class MoviesApi {
+  constructor({ baseUrl }) {
+    this._baseUrl = baseUrl;
+  }
+
+  async _requestResult(res) {
+    const result = await res.json();
+    return res.ok ? result : Promise.reject(res);
+  }
+
+  getMovies() {
+    return fetch(`${this._baseUrl}`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }).then((res) => this._requestResult(res));
+  }
+}
+
+const moviesApi = new MoviesApi({
+  baseUrl: MOVIES_URL,
+});
+
+export default moviesApi;
