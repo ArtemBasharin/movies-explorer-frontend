c7b404551adc6ebdf5e52ab8d0c0fd3fe48824f2
basharinaa <temon-bash@yandex.ru>
2022-11-15 18:55:30 +0600

    handy hooks added

---
+++ src/hooks/useEscapePress.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+
+export default function useEscapePress(callback, dependency) {
+  useEffect(() => {
+    if (dependency) {
+      const onEscClose = e => {
+        if (e.key === 'Escape') {
+          callback()
+        }
+      }
+      document.addEventListener('keyup', onEscClose);
+
+      return () => {
+        document.removeEventListener('keyup', onEscClose)
+      };
+    }
+  }, [callback, dependency])
+}
