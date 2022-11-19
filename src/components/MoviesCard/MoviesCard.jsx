75c5d6f2bb360abcecab53d6a92fa9fc32493577
basharinaa <temon-bash@yandex.ru>
2022-11-19 19:57:20 +0600

    update and refactor MoviesCard

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,40 +1,77 @@
-import './MoviesCard.css';
-import { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import "./MoviesCard.css";
+import { useLocation } from "react-router-dom";
+import { getHumanizedDuration } from "../../utils/utils.js";
+import mainApi from "../../api/MainApi";
+import { useContext } from "react";
+import PopupContext from "../../contexts/PopupContext";
+import SavedMoviesContext from "../../contexts/SavedMoviesContext";
 
-export default function MoviesCard({ card }) {
+export default function MoviesCard({
+  movie,
+  saved,
+  onLikeClick,
+}) {
   const location = useLocation();
+  const { setPopup } = useContext(PopupContext)
+  const { savedMovies, setSavedMovies } = useContext(SavedMoviesContext)
+  const isSavedMovies = location.pathname === '/saved-movies'
+  const title = isSavedMovies ? 'Удалить фильм из сохранённых' : `${saved ? 'Удалить фильм из сохранённых' : 'Сохранить фильм'}`
 
-  const [isCardSaved, setIsCardSaved] = useState(card.saved);
+  function handleLikeClick() {
+    onLikeClick(movie);
+  }
 
-  const handleOnClick = () => {
-    setIsCardSaved(!isCardSaved);
-  };
+  function handleDeleteMovie(movie) {
+    const savedMovie = savedMovies.find(item => item.movieId === movie.id || item.movieId === movie.movieId);
+
+    mainApi
+      .deleteMovie(savedMovie._id)
+      .then(() => {
+        const moviesWithoutDeleted = savedMovies.filter(m => !(movie.id === m.movieId || movie.movieId === m.movieId));
+
+        setSavedMovies(moviesWithoutDeleted);
+      })
+      .catch((err) =>
+        setPopup({
+          isOpen: true,
+          successful: false,
+          text: err,
+        })
+      );
+  }
+
+  function handleDeleteClick() {
+    handleDeleteMovie(movie);
+  }
 
   return (
     <li className="movies-card">
       <article className="movies-card__item">
+        <a target="_blank" rel="noreferrer" href={movie.trailerLink}>
+          <img
+            src={movie.image}
+            alt={movie.nameRU}
+            title={`Описание: ${movie.description}\n\nСнято: ${movie.country} ${movie.year}г.`}
+            className="movies-card__poster"
+          />
+        </a>
+
         <div className="movies-card__description">
-          <h2 className="movies-card__title">{card.title}</h2>
-          {location.pathname === '/movies' && (
-            <button
+          <h2 className="movies-card__title">{movie.nameRU}</h2>
+
+          <button
             type="button"
-            className={`movies-card__button movies-card__button_type_${
-              !isCardSaved ? 'save' : 'saved'
-            }`}
-            onClick={handleOnClick}
-            ></button>
-            )}
-          {location.pathname === "/saved-movies" && (
-            <button
-            type="button"
-            className="movies-card__button movies-card__button_type_unsave"
-            ></button>
-            )}
+            className={`movies-card__button movies-card__button_type_${isSavedMovies ? 'delete' : (saved ? 'saved' : 'save')}`}
+            onClick={isSavedMovies ? handleDeleteClick : (saved ? handleDeleteClick : handleLikeClick)}
+            aria-label={title}
+            title={title}
+          />
         </div>
-        <span className="movies-card__duration">{card.duration}</span>
-        <img src={card.poster} alt={card.title} className="movies-card__poster"/>
+
+        <span className="movies-card__duration">
+          {getHumanizedDuration(movie.duration)}
+        </span>
       </article>
     </li>
-  )
+  );
 }
