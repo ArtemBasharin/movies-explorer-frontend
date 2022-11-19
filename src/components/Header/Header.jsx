1ac0fabb3064bf6c36e6e7b01355db18773417d3
basharinaa <temon-bash@yandex.ru>
2022-11-19 19:13:17 +0600

    remove unnecessary code from Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,17 @@ import './Header.css';
 import Navigation from '../Navigation/Navigation.jsx';
 import logo from '../../images/logo.svg';
 
-function Header({ themeDark, authorized, onClickBurger, isBurgerOpened }) {
+export const headerEndpoints = ["/movies", "/saved-movies", "/profile", "/"];
+
+function Header() {
   return (
-    <header className={`header header_theme_${themeDark ? 'dark' : 'light'}`}>
+    <header className="header">
       <div className="header__container">
-        <Link to="/" className="header__link">
+        <Link to='/' className='header__link'>
           <img src={logo} alt="Логотип" />
         </Link>
-        <Navigation authorized={authorized} onClickBurger={onClickBurger} isBurgerOpened={isBurgerOpened} />
+
+        <Navigation/>
       </div>
     </header>
   );
