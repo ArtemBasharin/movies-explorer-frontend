7cd622d36bb260e09c97732661bbba62675ae29a
basharinaa <temon-bash@yandex.ru>
2022-11-24 01:16:26 +0600

    save all movies in localStorage

--- src/api/MoviesApi.js
+++ src/api/MoviesApi.js
@@ -11,10 +11,20 @@ class MoviesApi {
   }
 
   getMovies() {
+    const allMoviesLS = localStorage.getItem('ALL_MOVIES_LS_KEY')
+
+    if (allMoviesLS) return Promise.resolve(JSON.parse(allMoviesLS))
+
     return fetch(`${this._baseUrl}`, {
       method: "GET",
       headers: { "Content-Type": "application/json" },
-    }).then((res) => this._requestResult(res));
+    })
+      .then(res => this._requestResult(res))
+      .then(movies => {
+        localStorage.setItem('ALL_MOVIES_LS_KEY', JSON.stringify(movies))
+
+        return movies
+      });
   }
 }
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,7 @@
 export const SHORT_MOVIES_DURATION = 40;
 export const SEARCH_QUERY_LS_KEY = "searchQuery";
 export const SHORT_MOVIES_MODE_LS_KEY = "shortMoviesMode";
+export const ALL_MOVIES_LS_KEY = "allMovies";
 export const JWT_LS_KEY = "jwt";
 
 export const DEVICE_PARAMS = {
